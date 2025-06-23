a4ab74dfa7f1af7e22aff14ef3c0e529b3167d17
Zuza Gronska <zgronska@gmail.com>
2025-06-23 14:21:46 +0200

    Show alert if nothing found, min length instead of set length

--- src/main.js
+++ src/main.js
@@ -2,27 +2,33 @@ import words from 'an-array-of-english-words'
 
 const form = document.querySelector('form')
 const list = document.getElementById('result')
+const alert = document.getElementById('alert')
 
 const getWords = (letters, length, requiredLetter = null) => {
     return words
-        .filter((word) => word.length === length)
+        .filter((word) => word.length >= length)
         .filter((word) =>
             requiredLetter ? word.includes(requiredLetter) : true
         )
-        .filter((word) => letters.some((letter) => word.includes(letter)))
+        .filter((word) => letters.every((letter) => word.includes(letter)))
 }
 
 const onFormSubmit = (event) => {
     list.innerHTML = ''
+    alert.textContent = ''
     event.preventDefault()
     const formData = new FormData(form)
 
     const letters = Array.from(formData.get('letters'))
-    const length = formData.get('length') || letters.length
+    const length = formData.get('length') || 3
     const requiredLetter = formData.get('required-letter')
 
     const result = getWords(letters, length, requiredLetter)
-    list.innerHTML = result.map((word) => `<li>${word}</li>`).join('')
+    if (result.length === 0) {
+        alert.textContent = `No words found with the given parameters`
+    } else {
+        list.innerHTML = result.map((word) => `<li>${word}</li>`).join('')
+    }
 }
 
 form.addEventListener('submit', onFormSubmit)
