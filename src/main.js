09a358c9aafc0434db84555a78674843142c4c01
Zuza Gronska <zgronska@gmail.com>
2025-06-23 14:12:40 +0200

    `getWords` method

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
+import words from 'an-array-of-english-words'
+
 const form = document.querySelector('form')
 const list = document.getElementById('result')
 
 const getWords = (letters, length, requiredLetter = null) => {
-    return ['word1', 'word2', 'word3']
+    return words
+        .filter((word) => word.length === length)
+        .filter((word) =>
+            requiredLetter ? word.includes(requiredLetter) : true
+        )
+        .filter((word) => letters.some((letter) => word.includes(letter)))
 }
 
 const onFormSubmit = (event) => {
+    list.innerHTML = ''
     event.preventDefault()
     const formData = new FormData(form)
 
     const letters = Array.from(formData.get('letters'))
-    const length = formData.get('length')
+    const length = formData.get('length') || letters.length
     const requiredLetter = formData.get('required-letter')
 
     const result = getWords(letters, length, requiredLetter)
