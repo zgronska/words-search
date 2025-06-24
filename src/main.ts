82be79807e1ffe47beec0de79b87a12c9369dd56
Zuza Gronska <zgronska@gmail.com>
2025-06-24 14:49:57 +0200

    Base form validation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import '@fontsource-variable/quicksand/wght.css'
 import './reset.css'
 import './style.css'
 import words from 'an-array-of-english-words'
+import debounce from './utils/debounce'
 
 const form = document.querySelector('form') as HTMLFormElement
 const list = document.getElementById('result') as HTMLOListElement
 const alert = document.getElementById('alert') as HTMLParagraphElement
 const sorting = document.getElementById('sorting') as HTMLSelectElement
+const submit = document.querySelector('button') as HTMLButtonElement
 
 /**
  * Filters the list of words to those that
@@ -74,5 +76,52 @@ const sort = (event: Event) => {
     list.innerHTML = listItems.map((item) => item.outerHTML).join('')
 }
 
+/**
+ * Checks if the given input element has an error.
+ */
+const hasError = (input: HTMLInputElement): boolean => {
+    const value = input.value
+    const id = input.id
+
+    const validationEl = input.parentElement?.querySelector(
+        '[data-validation]'
+    ) as HTMLElement
+
+    if (!value) {
+        validationEl.textContent = input.required ? 'Field is required' : ''
+        return input.required
+    }
+
+    const mapping = {
+        'letters': {
+            regex: /^[a-zA-Z]+$/,
+            message: 'Enter only letters (a-z)'
+        },
+        'length': {
+            regex: /^(?:[3-9]|\d{2,}|\d{1}[0-9])$/,
+            message: 'The length must be a number greater than or equal 3'
+        },
+        'required-letter': {
+            regex: /^[a-zA-Z]$/,
+            message: 'Enter only one letter (a-z)'
+        }
+    }
+
+    const error = !mapping[id as keyof typeof mapping].regex.test(value)
+
+    validationEl.textContent = error
+        ? mapping[id as keyof typeof mapping].message
+        : ''
+
+    return error
+}
+
 form.addEventListener('submit', onFormSubmit)
+form.addEventListener(
+    'input',
+    debounce(function (e: Event) {
+        const target = e.target as HTMLInputElement
+        submit.toggleAttribute('disabled', hasError(target))
+    })
+)
 sorting.addEventListener('change', sort)
